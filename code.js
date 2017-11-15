bf77d354f6576c446b7ab28613bee1c71859827a
Jordan Hay <jordanhay0112@gmail.com>
2017-11-15 17:46:31 +1300

    Login removal or so I say

--- code.js
+++ code.js
@@ -1,11 +1,31 @@
-function TheLogin() {
+document.onkeydown = checkKey;
 
-    var password = 'genericpasscode';
+var x = 0;
+var y = 0;
+var z = 0;
 
-    if (this.document.login.pass.value == password) {
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '88') {
+        x = x + 1;
+    }
+    
+    if (e.keyCode == '89') {
+        y = y + 1;
+    }
+    
+    if (e.keyCode == '90') {
+        z = z + 1;
+    }
+    
+    if (x == 5 && y == 5 && z == 5) {
         top.location.href="sites.html";
     }
-    else {
-        window.alert("Incorrect password, please try again.");
+    
+    if (x > 5||y > 5||z > 5) {
+        top.location.href="index.html";
     }
+
 }
