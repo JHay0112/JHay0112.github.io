f19cf9f1d690ec36afc7bf3f630315cff5d094e0
Jordan Hay <jordanhay0112@gmail.com>
2017-12-13 21:27:29 +1300

    Dev Blog Search

--- scripts/code.js
+++ scripts/code.js
@@ -111,3 +111,22 @@ function newBanner() {
     var bannerNumber = Math.floor(Math.random() * (banners.length));
     document.getElementById('banner').innerHTML = banners[bannerNumber];
 }
+
+function searchFunction() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("searchTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        } 
+    }
+}
