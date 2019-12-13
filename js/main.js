faabd7b1fc89a5339e4a9efcf9e9feb235685d1f
JHay0112 <jordanhay0112@gmail.com>
2019-12-13 18:22:31 +1300

    Banner

--- js/main.js
+++ js/main.js
@@ -7,7 +7,9 @@
 function exitLoadingScreen() {
     loadingScreen = document.getElementById("loading-screen"); // Get loading screen
     loadingSpinner = document.getElementById("loading-spinner"); // Get loading spinner
+    main = document.getElementById("main"); // Get main content
 
-    loadingScreen.style.animation = "fade-out 2s 1s forwards ease"; // Fade out
+    loadingScreen.style.animation = "fade-out 1s 1s forwards ease"; // Fade out
     loadingSpinner.style.animation = "spin 1s infinite linear, fade-out 2s forwards"; // Fade out while still spinning
+    main.style.animation = "fade-in 1s 2s forwards ease";
 }
