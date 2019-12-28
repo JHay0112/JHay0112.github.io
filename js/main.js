60eef6715ae16512e641412c4b45c7dede28d239
JHay0112 <jordanhay0112@gmail.com>
2019-12-28 15:38:01 +1300

    Some more slight exit loading screen tweaks

--- js/main.js
+++ js/main.js
@@ -29,8 +29,8 @@ async function exitLoadingScreen() {
 
     loadingLogo.style.animation = "fade-out 1s 1s forwards ease"; // Fade out
     loadingSpinner.style.animation = "spin 1s infinite linear, fade-out 2s forwards"; // Fade out while still spinning
-    loadingScreen.style.animation = "slide-out-bottom 1.1s 2s forwards ease-out";
-    await sleep(2000);
+    loadingScreen.style.animation = "slide-out-bottom 1.3s 2s forwards ease-out";
+    await sleep(1900);
     document.body.style.overflowY = "auto";
 }
 
