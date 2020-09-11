7eaddb7a1540e5f1c0f36eae25f4ca487f02826b
Jordan Hay <jordanhay0112@gmail.com>
2020-09-11 20:59:28 +1200

    Building blocks of the new design

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@
     Author: Jordan Hay
 */
 
+// Globals
+var nav = document.getElementById("nav"); // Get the nav
+var sticky = nav.offsetTop; // Get the offset position of the nav
+
 // Functions
 // Sleep function
 function sleep(ms) {
@@ -24,7 +28,7 @@ async function exitLoadingScreen() {
 
 // Runs when the user scrolls, used in stickynavs etc
 function scrollFunction() {
-    // Blank for now
+    stickyNav();
 }
 
 // Behold! Bees
@@ -39,6 +43,74 @@ async function newBee(e) {
         document.body.removeChild(bee);
     }
 }
+// Slideshow
+async function runSlideShow() {
+
+    var imgNum = 3; // Amount of images in  
+
+    header = document.getElementById("header");
+    pseudoHeader = document.getElementById("pseudo-header");
+
+    var img = 0;
+
+    while(true) {
+
+        prevImg = img;
+
+        // Stops getting the same image twice in a row
+        while(prevImg === img) {
+            img = Math.floor(Math.random() * (imgNum - 1) + 1);
+        }
+
+        pseudoHeader.style.backgroundImage = "url(\"img/header/".concat(img, ".jpg\")");
+        await sleep(500);
+        pseudoHeader.style.visibility = "visible";
+        pseudoHeader.style.animation = "slideshow-new-slide 1s ease";
+        await sleep(1000);
+        header.style.backgroundImage = "url(\"img/header/".concat(img, ".jpg\")");
+        pseudoHeader.style.animation = "";
+        pseudoHeader.style.visibility = "hidden";
+        await sleep(7000);
+    }
+
+}
+
+// Add the sticky class to the nav when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNav() {
+
+    content = document.getElementById("content");
+    content.style.position = "relative";
+
+    if (window.pageYOffset >= sticky) {
+        nav.classList.add("stick")
+        if(nav.classList.contains("responsive")) {
+            content.style.top = nav.scrollHeight + "px";
+        } else {
+            content.style.top = "60px";
+        }
+    } else {
+        nav.classList.remove("stick");
+        content.style.top = "";
+    }
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function toggleResponsiveNav() {
+
+    navAnchors = document.querySelectorAll(".anchor");
+
+    if (!nav.classList.contains("responsive")) {
+        nav.classList.add("responsive");
+        nav.style.height = nav.scrollHeight + "px";
+        for(i = 0; i < navAnchors.length; i++) {navAnchors[i].style.top = -nav.scrollHeight - 10 + "px";}
+    } else {
+        nav.classList.remove("responsive");
+        nav.style.height = "60px";
+        for(i = 0; i < navAnchors.length; i++) {navAnchors[i].style.top = -60 - 10 + "px";}
+    }
+
+    stickyNav();
+}
 
 // Event listeners (or similar)
 window.onscroll = function() {scrollFunction()}; // Run the scroll function anytime the user scrolls
@@ -48,3 +120,6 @@ document.addEventListener("copy", (event) => {
     event.clipboardData.setData('text', document.getSelection() + pagelink);
     event.preventDefault();
 });
+
+// MenuSpy
+var ms = new MenuSpy(document.querySelector("#nav-header"));
